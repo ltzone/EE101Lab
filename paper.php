8905d8df3ceebe6b07052d48bb00ad0a665f3afa
ltzone <ltzhou@sjtu.edu.cn>
2019-05-27 18:58:45 +0800

    paper_reference demo 0527

--- paper.php
+++ paper.php
@@ -23,7 +23,7 @@
 <div class="container">
 	<?php
 		$paper_id = $_GET["paper_id"];
-		$link = mysqli_connect("localhost:3306", 'root', '770528', 'FINAL');
+		$link = mysqli_connect("localhost:3306", 'root', '', 'FINAL');
 		
 		$result = mysqli_query($link, "SELECT Title from papers where PaperID='$paper_id'");
 		if ($result) {
@@ -111,70 +111,16 @@
 	
 		#reference查找
 
-		$paper_id = $_GET["paper_id"];
-		$reference_id=mysqli_query($link, "SELECT ReferenceID from paper_reference2 where PaperID='$paper_id'");
-		$result = mysqli_query($link, "SELECT Title from papers where PaperID='$reference_id'");			
-			//$result = mysqli_query($link, "SELECT PaperID from paper_author_affiliation where AuthorID='$author_id'");
-		if ($result) {
-			echo "<table border=\"0\" frame=\"hsides\"><tr><th>Reference Title</th><th>Authors</th><th>Conference</th></tr>";
-			while ($row = mysqli_fetch_array($result)) {
-				echo "<tr>";
-				$paper_id = $row['PaperID'];
-				# 请增加对mysqli_query查询结果是否为空的判断
-				$paper_info = mysqli_fetch_array(mysqli_query($link, "SELECT Title, ConferenceID from papers where PaperID='$paper_id'"));
-				if ($paper_info){
-					$paper_title = $paper_info['Title'];
-					$conf_id = $paper_info['ConferenceID'];
-					$paper_title2 = ucwords($paper_title);
-					echo "<td>$paper_title2</td>";
+		#仅取查询结果中的第一个元素作为demo，实现的时候可以用循环全部展示
+		#请增加查询reference结果为空的条件判断  --ltzhou
 
-					$author_info = mysqli_query($link, "SELECT A.AuthorID, AuthorName FROM paper_author_affiliation A LEFT JOIN authors B ON A.AuthorID = B.AuthorID WHERE PaperID = '$paper_id' ORDER BY AuthorSequence ASC");
+		$reference_id=mysqli_fetch_all(mysqli_query($link, "SELECT ReferenceID from paper_reference2 where PaperID='$paper_id'"));
+		$reference_id1 = $reference_id[0][0];
+		$result = mysqli_fetch_all(mysqli_query($link, "SELECT Title from papers where PaperID='$reference_id1'"));
+		$reference_name1 = $result[0][0];
+		echo "<pre>"; echo $reference_name1; echo "</pre>";
 
-					echo "<td>";
-					while ($author_row = mysqli_fetch_array($author_info)){
-						$author_name = $author_row['AuthorName'];
-						$author_another_id = $author_row['AuthorID'];
-						$author_name2 = ucwords($author_name);
-						$author_another_id2 = ucwords($author_another_id);
-						echo "<a href=\"author.php?author_id=$author_another_id2\">$author_name2; </a>";
-					}
-					echo "</td>";
-
-
-					echo "<td>";
-					$conference_row = mysqli_fetch_array(mysqli_query($link, "SELECT ConferenceName from conferences WHERE ConferenceID = '$conf_id'"));
-					$conference_name = $conference_row['ConferenceName'];
-					$conference_name2 = ucwords($conference_name);
-					echo $conference_name2;
-					echo "</td>";
-
-
-				}
-				# 请增加根据paper id在PaperAuthorAffiliations与Authors两个表中进行联合查询，找到根据AuthorSequenceNumber排序的作者列表，并且显示出来的部分
-
-
-				# 请补充根据$conf_id查询conference name并显示的部分
-
-				echo "</tr>";
-			}
-			echo "</table>";
-
-
-
-
-
-
-
-
-		} else {
-			echo "Paper not found";
-		}
-
-
-
-
-
-
+		
 
 
 	?>
