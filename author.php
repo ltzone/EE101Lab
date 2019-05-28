42fe3b89ea06040c7dcf5c1e7679570e52d2a3d0
ltzone <ltzhou@sjtu.edu.cn>
2019-05-28 14:56:44 +0800

    MISSION distributed2 0528

--- author.php
+++ author.php
@@ -5,7 +5,6 @@
 <meta http-equiv="content-type" content="text/html; charset=gbk" />
     <meta charset="utf-8">
     <script src="js/echarts.js"></script>
-    <script src="js/mycharts.js"></script>
     <script src="https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link href="css/style.css" rel="stylesheet" type="text/css" />
@@ -171,6 +170,10 @@
 			echo "<div id=\"main\" style=\"width:600px; height: 400px;\"></div>";
 			echo "<script src=\"js/mycharts.js\"> conference_graph($conference_counts,$conference_names);
 			//</script>";
+
+
+			# 有关echarts会议的统计数据
+			$result = mysqli_query($link,"SELECT count(*) AS Yearcount, PaperPublishYear FROM (paper_author_affiliation C INNER JOIN papers D ON D.PaperID = C.PaperID) WHERE C.AuthorID = '".$' GROUP BY PaperPublishYear order by PaperPublishYear asc;
 		}
 		} else {
 			echo "Name not found";
