68a54632da0b7254f457e8e26152ead41bc50b87
KireiTC <hongbo_yang@sjtu.edu.cn>
2019-05-30 21:23:35 +0800

    paper写好了，可是不怎么好看，not found 输出仍然有问题

--- paper.php
+++ paper.php
@@ -83,14 +83,108 @@
 	
 		#reference查找
 
-		#仅取查询结果中的第一个元素作为demo，实现的时候可以用循环全部展示
-		#请增加查询reference结果为空的条件判断  --ltzhou
+		
+		#增加查询reference结果为空的条件判断（bug）
 
-		$reference_id=mysqli_fetch_all(mysqli_query($link, "SELECT ReferenceID from paper_reference2 where PaperID='$paper_id'"));
-		$reference_id1 = $reference_id[0][0];
-		$result = mysqli_fetch_all(mysqli_query($link, "SELECT Title from papers where PaperID='$reference_id1'"));
-		$reference_name1 = $result[0][0];
-		echo "<pre>"; echo $reference_name1; echo "</pre>";
+		
+		echo "<h1 style=\"font-family:Arial Black\">被引用</h1>";
+		$result = mysqli_query($link, "SELECT ReferenceID from paper_reference2 where PaperID='$paper_id'");
+		// 显示搜索结果的分区
+		if ($result) {
+			echo "<div class='paperlis'>";	
+			while ($row = mysqli_fetch_array($result)) {
+				$paper_id_ref = $row['ReferenceID'];
+				$paper_info = mysqli_fetch_array(mysqli_query($link, "SELECT Title, ConferenceID from papers where PaperID='$paper_id_ref'"));
+				if ($paper_info){
+					$paper_title = $paper_info['Title'];
+					$conf_id = $paper_info['ConferenceID'];
+					$paper_title2 = ucwords($paper_title);
+					echo "<a href=\"paper.php?paper_id=$paper_id_ref\"><h3>$paper_title2</h3></a>";
+					echo "<table>";
+					echo "<tr><td width = '120'><b> Authors: </b></td><td>";
+					$author_info = mysqli_query($link, "SELECT A.AuthorID, AuthorName FROM paper_author_affiliation A LEFT JOIN authors B ON A.AuthorID = B.AuthorID WHERE PaperID = '$paper_id' ORDER BY AuthorSequence ASC");
+
+					while ($author_row = mysqli_fetch_array($author_info)){
+						$author_name = $author_row['AuthorName'];
+						$author_another_id = $author_row['AuthorID'];
+						$author_name2 = ucwords($author_name);
+						$author_another_id2 = ucwords($author_another_id);
+						echo "<a href=\"author.php?page=1&author_id=$author_another_id2\">$author_name2</a>";
+						echo "; ";
+					}
+					echo "</td></tr>";
+					echo "<tr><td><b> Conference: </b></td><td>";
+
+					$conference_row = mysqli_fetch_array(mysqli_query($link, "SELECT ConferenceName from conferences WHERE ConferenceID = '$conf_id'"));
+					$conference_name = $conference_row['ConferenceName'];
+					$conference_name2 = ucwords($conference_name);
+					echo "<a href=\"conference.php?page=1&conference_id=$conf_id\">$conference_name2</a>";
+					echo "</td></tr>";
+					echo "</table>";
+				}
+				
+				else {
+				echo "Reference not found";//这里为啥不出来……
+		}
+
+				echo "<hr>";
+			}
+		}
+		
+		
+		
+		
+		
+		echo "<h1 style=\"font-family:Arial Black\">引用文章推荐</h1>";
+				
+
+		$result = mysqli_query($link, "SELECT PaperID from paper_reference2 where ReferenceID='$paper_id'");
+		// 显示搜索结果的分区
+		if ($result) {
+			echo "<div class='paperlis'>";	
+			while ($row = mysqli_fetch_array($result)) {
+				$paper_id_ref = $row['PaperID'];
+				$paper_info = mysqli_fetch_array(mysqli_query($link, "SELECT Title, ConferenceID from papers where PaperID='$paper_id_ref'"));
+				if ($paper_info){
+					$paper_title = $paper_info['Title'];
+					$conf_id = $paper_info['ConferenceID'];
+					$paper_title2 = ucwords($paper_title);
+					echo "<a href=\"paper.php?paper_id=$paper_id_ref\"><h3>$paper_title2</h3></a>";
+					echo "<table>";
+					echo "<tr><td width = '120'><b> Authors: </b></td><td>";
+					$author_info = mysqli_query($link, "SELECT A.AuthorID, AuthorName FROM paper_author_affiliation A LEFT JOIN authors B ON A.AuthorID = B.AuthorID WHERE PaperID = '$paper_id' ORDER BY AuthorSequence ASC");
+
+					while ($author_row = mysqli_fetch_array($author_info)){
+						$author_name = $author_row['AuthorName'];
+						$author_another_id = $author_row['AuthorID'];
+						$author_name2 = ucwords($author_name);
+						$author_another_id2 = ucwords($author_another_id);
+						echo "<a href=\"author.php?page=1&author_id=$author_another_id2\">$author_name2</a>";
+						echo "; ";
+					}
+					echo "</td></tr>";
+					echo "<tr><td><b> Conference: </b></td><td>";
+
+					$conference_row = mysqli_fetch_array(mysqli_query($link, "SELECT ConferenceName from conferences WHERE ConferenceID = '$conf_id'"));
+					$conference_name = $conference_row['ConferenceName'];
+					$conference_name2 = ucwords($conference_name);
+					echo "<a href=\"conference.php?page=1&conference_id=$conf_id\">$conference_name2</a>";
+					echo "</td></tr>";
+					echo "</table>";
+				}
+				
+				else {
+				echo "Reference not found";//这里为啥不出来……
+		}
+
+				echo "<hr>";
+			}
+		}
+		
+		
+		
+		
+		
 
 	?>
 </div>
