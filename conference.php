b9a2b2c7928da116b3845a719d97519388098636
dongfeng12 <353873992@qq.com>
2019-06-02 11:17:45 +0800

    sql sentence requires optimizing

--- conference.php
+++ conference.php
@@ -5,6 +5,8 @@
 <link href="css/style.css" rel="stylesheet" type="text/css" />
 <title>Conference Page</title>
 
+<script src="https://cdn.bootcss.com/echarts/4.2.1-rc1/echarts-en.common.js"></script>
+
 </head>
 
 <body>
@@ -17,6 +19,7 @@
 		 </div>
 		</div>
 	</div>
+
 
 <div class="container">
 	<?php
@@ -39,6 +42,34 @@
 		$page_total=(integer)(($num_results+9)/10);
 
 
+
+		//创建$years,$number
+		$years=array();
+		$data=array();
+		$number1=array();
+		$result = mysqli_query($link, "SELECT PaperID from papers where ConferenceID='$conference_id' ");
+		if($result) {
+			while($row = mysqli_fetch_array($result)){
+				$paper_id = $row['PaperID'];
+				$paper_info = mysqli_fetch_array(mysqli_query($link, "SELECT PaperPublishYear from papers where PaperID='$paper_id'"));
+				if(array_key_exists($paper_info['PaperPublishYear'], $data)){
+					$data[$paper_info['PaperPublishYear']]++;
+				}else{
+					$data[$paper_info['PaperPublishYear']]=1;
+				}
+			}
+		}
+		//创建years,number1
+		ksort($data);
+		foreach ($data as $key => $value) {
+			$years[]=$key;
+			$number[]=$value;
+		}
+
+
+
+
+
 		$result = mysqli_query($link, "SELECT PaperID from papers where ConferenceID='$conference_id'limit ".(($page_num-1)*10).",10 ");
 		// 显示搜索结果的分区	
 		if ($result) {
@@ -47,7 +78,7 @@
 			while ($row = mysqli_fetch_array($result)) {
 				echo "<tr>";
 				$paper_id = $row['PaperID'];
-				$paper_info = mysqli_fetch_array(mysqli_query($link, "SELECT Title, ConferenceID from papers where PaperID='$paper_id'"));
+				$paper_info = mysqli_fetch_array(mysqli_query($link, "SELECT Title, ConferenceID, PaperPublishYear from papers where PaperID='$paper_id'"));
 				if ($paper_info){
 					$paper_title = $paper_info['Title'];
 					$conf_id = $paper_info['ConferenceID'];
@@ -124,15 +155,62 @@
 			echo "</div>";
 		}
 
-			// 展示echarts的分区
-		   	echo "<div class='chartlis'>";
-			echo "</div>";
+		
+		// 展示echarts的分区
+		echo "<div id=\"main\" style=\"width: 600px;height:400px;\"></div>";
+
+		echo "<div class='chartlis'>";
+		echo "</div>";
 
 		} else {
 			echo "Name not found";
 		}
 
 	?>
+
+	
+	<!-- echarts画图，需要数组 years1,number1 -->
+	<!--div id="main" style="width: 600px;height:400px;"></div-->
+    <script type="text/javascript">
+        // 基于准备好的dom，初始化echarts实例
+        var myChart = echarts.init(document.getElementById('main'));
+
+        var years1 = eval(decodeURIComponent('<?php echo urlencode(json_encode($years));?>'));
+        var number1 = eval(decodeURIComponent('<?php echo urlencode(json_encode($number));?>'));
+ 
+        // 指定图表的配置项和数据
+		option = {
+		    title: {
+		        text: '论文发表数量图'
+		    },
+		    tooltip: {
+		        trigger: 'axis'
+		    },
+		    legend: {
+		        data:['number of papers']
+		    },
+		    xAxis: {
+		        type: 'category',
+		        data: years1
+		    },
+		    yAxis: {
+		        type: 'value'
+		    },
+		    series: [
+		    {
+	            name:'papers',
+		        type: 'line',
+		        data: number1
+		    },
+		    ]
+		};
+		
+        // 使用刚指定的配置项和数据显示图表。
+        myChart.setOption(option);
+    </script>
+
+
+
 </div>
 </body>
 </html>
