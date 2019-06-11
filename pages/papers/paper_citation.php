0127dfe59160e64af8104a4ebf1633b4877a77f0
ltzone <ltzhou@sjtu.edu.cn>
2019-06-11 13:14:12 +0800

    title update

--- pages/papers/paper_citation.php
+++ pages/papers/paper_citation.php
@@ -3,7 +3,7 @@
 <html lang="en">
   <head>
     <meta charset="utf-8" />
-    <title>Dashboard - Bootstrap Admin</title>
+    <title>Paper - Paper Citation</title>
     
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <meta name="apple-mobile-web-app-capable" content="yes" />    
